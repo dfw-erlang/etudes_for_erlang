73b4dd65905d34d36acfaaf67f848fdd5495c736
Steven Proctor <steven.proctor@gmail.com>
2013-10-29 20:01:29 -0500

    Etude 4-4 completed\!

--- chapter4/powers.erl
+++ chapter4/powers.erl
@@ -4,9 +4,13 @@
 
 raise(_, 0) -> 1;
 
-raise(X, 1) -> X;
-
-raise(X, N) when N > 0 -> X * raise(X, N-1);
+raise(X, N) when N > 0 ->
+  raise(X, N, 1);
 
 raise(X, N) when N < 0 -> 1 / raise(X, -N).
 
+%Non-Exported functions
+raise(_, 0, Accum) -> Accum;
+
+raise(X, N, Accum) ->
+  raise(X, N-1, X * Accum).
